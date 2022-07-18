3500d87f7f238437428355419168bef2a3b7da77
unknown <wabanasali@gmail.com>
2022-07-18 13:16:56 +0100

    django query sets and managers assignment done

--- src/links/models.py
+++ src/links/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+
+from links.managers import ActiveLinkManager
 # Create your models here.
 class Link(models.Model):
     target_url = models.URLField(max_length=200)
@@ -10,5 +12,8 @@ class Link(models.Model):
     )
     created_date = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True)
+
+    objects = models.Manager()
+    public = ActiveLinkManager()
 
     
--- src/links/urls.py
+++ src/links/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("update/<int:pk>", views.PostUpdateApi.as_view(), name="api_update"),
     path("delete/<int:pk>", views.PostDeleteApi.as_view(), name="api_delete"),
     path("", views.PostListApi.as_view(), name="api_list"),
+    path("active/", views.ActiveLinkView.as_view(), name="active_link"),
+    path("recent/", views.RecentLinkView.as_view(), name="recent_link")
 ]
